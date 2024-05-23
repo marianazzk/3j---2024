707bb1c1723e839842de17088f7d14d90e694e76
marianazzk <106184155+marianazzk@users.noreply.github.com>
2024-05-23 08:12:33 -0300

    MARIANA -  23-05

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const botoes = document.querySelector(".botao")
-console. log(botoes)
+const botoes = document.querySelectorAll(".botao")
+console.log(botoes);
 for (let i = 0; i < botoes.length ; i++){
-  botoes[i]. onclik = function(){
-    for(j=0; j<botoes.length;j++){
+  botoes[i].onclick = function(){
+    for(let j=0; j<botoes.length;j++){
     botoes[j].classList.remove("ativo")
     }
-    botoes[j].classList.add("ativo")
+    botoes[i].classList.add("ativo")
     }
 }
 
